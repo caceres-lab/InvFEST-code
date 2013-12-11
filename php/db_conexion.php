1e4064d395f2371be002596edfadacfab2342f1a
Lorena Pantano <lorena.pantano@gmail.com>
2013-12-11 13:19:34 +0100

    some changes in DB

--- php/db_conexion.php
+++ php/db_conexion.php
@@ -7,21 +7,21 @@ if ($_SESSION["autentificado"]=='SI') {
 		// Admin user
 		$user = "invfest";
 		$password = "pwdInvFEST";
-		$db = "INVFEST-DB-dev";
+		$db = "INVFEST-DB";
 	
 	} elseif ($_SESSION['MySQLuser']=="invfestdb-lab") {
 	
 		// Lab user
 		$user = "invfestdb-lab";
 		$password = "InvFESTLab";
-		$db = "INVFEST-DB-dev";
+		$db = "INVFEST-DB";
 	
 	} else {
 	
 		// I don't know who you are...
 		$user = "invfest";
 		$password = "pwdInvFEST";
-		$db = "INVFEST-DB-PUBLIC-dev";
+		$db = "INVFEST-DB-PUBLIC";
 	
 	}
 
@@ -30,7 +30,7 @@ if ($_SESSION["autentificado"]=='SI') {
 		// No login
 		$user = "invfest";
 		$password = "pwdInvFEST";
-		$db = "INVFEST-DB-PUBLIC-dev";
+		$db = "INVFEST-DB-PUBLIC";
 
 }
 
