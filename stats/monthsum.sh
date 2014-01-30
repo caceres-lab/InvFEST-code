84b18d8e3c8f1ec2e825a0920c2df376d7655db4
Lorena Pantano <lorena.pantano@gmail.com>
2014-01-30 12:08:00 +0100

    add statistic by month

--- stats/monthsum.sh
+++ stats/monthsum.sh
@@ -7,6 +7,7 @@ m=$(date +'%m')
 DATE=$y.$m
 #get num hits per day
 sort -u hits.txt | cut -d " " -f 2,3,4 | uniq -c > $DATE.xday.txt
+sort -u hits.txt | cut -d " " -f 2,3,4 | uniq -c | awk '{i=i+$1} END {print i}' >> $y.xmonth.txt
 #do bzip hits
 bzip2 -zkv --best hits.txt
 mv hits.txt.bz2 $DATE.raw.bz2
